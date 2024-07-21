777c2891c166c853499135acf00d73a99e78b6d0
HarshaVardhan <harshavardhan1617@gmail.com>
2024-07-21 22:14:10 +0530

    integrated api with app state at init

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
 import 'coin_data.dart';
+import 'dart:io' show Platform;
+import 'secrets.dart';
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -10,19 +17,80 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  List<DropdownMenuEntry<String>> getDropdownItems() {
-    List<DropdownMenuEntry<String>> dropdownItem = [];
+  String selectedCurrency = 'USD';
+  String selectedCoin = 'BTC';
+  String value = '?';
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
     for (String currency in currenciesList) {
-      var newItem = DropdownMenuEntry(
-        label: currency,
+      var newItem = DropdownMenuItem(
         value: currency,
+        child: Text(currency),
       );
-      dropdownItem.add(newItem);
+      dropdownItems.add(newItem);
     }
-    return dropdownItem;
+
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value!;
+        });
+      },
+    );
   }
 
-  String? selectedCurrency = 'INR';
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
+  void printType(dynamic variable) {
+    print("The type of the variable is: ${variable.runtimeType}");
+  }
+
+  //TODO: Create a method here called getData() to get the coin data from coin_data.dart
+  void getData(String coin, String currency) async {
+    Uri url =
+        Uri.parse('https://rest.coinapi.io/v1/exchangerate/$coin/$currency');
+    try {
+      final response = await http
+          .get(url, headers: {'Accept': 'text/plain', 'X-CoinAPI-Key': apiKey});
+
+      if (response.statusCode == 200) {
+        final decodedResponse = await jsonDecode(response.body);
+        String rate = decodedResponse['rate'].toStringAsFixed(2);
+        setState(() {
+          value = rate;
+        });
+      } else {
+        print('error fetching data ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    //TODO: Call getData() when the screen loads up.
+    getData(selectedCoin, selectedCurrency);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +113,8 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  //TODO: Update the Text Widget with the live bitcoin data here.
+                  '1 BTC = $value USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -60,15 +129,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownMenu<String>(
-              initialSelection: selectedCurrency,
-              dropdownMenuEntries: getDropdownItems(),
-              onSelected: (value) {
-                setState(() {
-                  selectedCurrency = value;
-                });
-              },
-            ),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
